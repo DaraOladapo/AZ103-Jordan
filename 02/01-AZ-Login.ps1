cca4f35ef3685362a44f1d20268aecb8b24c325d
Dara Oladapo <daraoladapo@outlook.com>
2020-05-11 13:17:46 +0100

    +VM Tasks

--- 02/01-AZ-Login.ps1
+++ 02/01-AZ-Login.ps1
@@ -1,9 +1,9 @@
-$SubscriptionId = ''
-
-az login
-
+#$SubscriptionId = ''
+##az login
 # Set subscription 
 #Set-AzContext -SubscriptionId $subscriptionId
-
 #or 
-az account set --subscription $SubscriptionId
+#az account set --subscription $SubscriptionId
+
+#Connect Azure Powershell to your Azure Account
+Connect-AzAccount
